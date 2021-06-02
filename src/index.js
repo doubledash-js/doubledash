a32a125c57f8f805fe08d68e63100f58d5bd40d8
Demetri <dmetri333@gmail.com>
2021-06-02 16:57:15 -0400

    Updated to web pack building

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-export { default as lang } from './lang';
-export { default as dom } from './dom';
-export { default as event } from './event';
-export { default as template } from './template';
-export { default as animate } from './animate';
+import lang from './lang';
+import dom from './dom';
+import event from './event';
+import template from './template';
+import animate from './animate';
+
+export {
+    lang,
+    dom,
+    event,
+    template,
+    animate
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.js',
+  entry: './src/webpack-index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'doubledash.js',
