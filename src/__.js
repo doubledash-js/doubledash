45344487c3dc7371dc7f7bae4437454ca6bd2d83
Demetri <dmetri333@gmail.com>
2021-06-03 12:11:28 -0400

    Added treeshaking

---
+++ src/__.js
@@ -0,0 +1,13 @@
+import lang from './lang';
+import dom from './dom';
+import event from './event';
+import template from './template';
+import animate from './animate';
+
+export default {
+    lang,
+    dom,
+    event,
+    template,
+    animate
+}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,78 @@
-import lang from './lang';
-import dom from './dom';
-import event from './event';
-import template from './template';
-import animate from './animate';
+// animate
+export { default as animate } from './animate/animate';
+export { default as bounce } from './animate/bounce';
+export { default as bounceIn } from './animate/bounceIn';
+export { default as bounceOut } from './animate/bounceOut';
+export { default as fadeIn } from './animate/fadeIn';
+export { default as fadeOut } from './animate/fadeOut';
+export { default as flash } from './animate/flash';
+export { default as heartBeat } from './animate/heartBeat';
+export { default as jello } from './animate/jello';
+export { default as pulse } from './animate/pulse';
+export { default as rotateIn } from './animate/rotateIn';
+export { default as rotateOut } from './animate/rotateOut';
+export { default as rubberBand } from './animate/rubberBand';
+export { default as shake } from './animate/shake';
+export { default as slideIn } from './animate/slideIn';
+export { default as slideOut } from './animate/slideOut';
+export { default as swing } from './animate/swing';
+export { default as tada } from './animate/tada';
+export { default as wobble } from './animate/wobble';
+export { default as zoomingIn } from './animate/zoomingIn';
+export { default as zoomingOut } from './animate/zoomingOut';
 
-export {
-    lang,
-    dom,
-    event,
-    template,
-    animate
-};
+//dom
+export { default as after } from './dom/after';
+export { default as append } from './dom/append';
+export { default as before } from './dom/before';
+export { default as children } from './dom/children';
+export { default as clone } from './dom/clone';
+export { default as contains } from './dom/contains';
+export { default as create } from './dom/create';
+export { default as index } from './dom/elementIndex';
+export { default as empty } from './dom/empty';
+export { default as find } from './dom/find';
+export { default as findOne } from './dom/findOne';
+export { default as hide } from './dom/hide';
+export { default as next } from './dom/next';
+export { default as parents } from './dom/parents';
+export { default as prepend } from './dom/prepend';
+export { default as prev } from './dom/prev';
+export { default as remove } from './dom/remove';
+export { default as show } from './dom/show';
+export { default as siblings } from './dom/siblings';
 
-export default {
-    lang,
-    dom,
-    event,
-    template,
-    animate
-}
+//event
+export { default as once } from './event/once';
+export { default as on } from './event/on';
+export { default as off } from './event/off';
+export { default as trigger } from './event/trigger';
+
+// Lang
+export { default as escapeHtml } from './lang/escapeHtml';
+export { default as extend } from './lang/extend';
+export { default as isArray } from './lang/isArray';
+export { default as isBoolean } from './lang/isBoolean';
+export { default as isEmpty } from './lang/isEmpty';
+export { default as isFunction } from './lang/isFunction';
+export { default as isJson } from './lang/isJson';
+export { default as isNull } from './lang/isNull';
+export { default as isNumber } from './lang/isNumber';
+export { default as isObject } from './lang/isObject';
+export { default as isRegExp } from './lang/isRegExp';
+export { default as isSet } from './lang/isSet';
+export { default as isString } from './lang/isString';
+export { default as isUndefined } from './lang/isUndefined';
+
+// template
+export { default as compile } from './template/compile';
+
+// category
+export { default as lang } from './lang';
+export { default as dom } from './dom';
+export { default as event } from './event';
+export { default as template } from './template';
+export { default as animate } from './animate';
+
+// all
+export { default } from './__.js';
